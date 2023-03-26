e16e79b5e6d1ebe455e962fae540f108ed460253
Josh Santos <josh@omnidapps.com>
2023-03-25 22:03:03 -0400

    macOS ansible automation

--- install.sh
+++ install.sh
@@ -6,81 +6,41 @@
 # TODO: Add full compatibility outside of OS X environments (could be part of migration to ansible instead)
 #######
 
-function setupZsh {
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    mkdir -p /usr/local/bin
-    sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-    cp .zshrc ~
-    sudo chsh -s "$(which zsh)" $USER
-}
-
-function setupVim {
-    cp -rf .vim ~
-    cp .vimrc ~
-    asdf plugin add nodejs
-    asdf install nodejs lts
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-    # Ref: https://github.com/junegunn/vim-plug/issues/225#issuecomment-696765599
-    vim +PlugInstall +qall >/dev/null 2>&1
-}
-
-function setupDeps {
-    if [ $machine == "Mac" ]; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        brew tap homebrew/cask-fonts
-        brew install --cask font-hack-nerd-font
-        brew install neofetch exa bat tmux the_silver_searcher
+function prepare {
+    # Supported only on Linux/macOS for now.
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+        export PATH="$HOME/.local/bin:$PATH"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        installXCodeTools
+        pip3 install --upgrade pip
     else
-      mkdir -p ~/.local/share/fonts
-      cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf && cd -
-      fc-cache -v
+        # Unknown.
+        echo "$OSTYPE is not yet supported. Exiting..."; exit 1;
     fi
 
-    if [ "$machine" == "Linux" ]; then
-      distro="$(cat /etc/os-release | grep -e "^NAME=" | awk -F "=" '{ print $2}' | sed 's/\"//g')"
-      case "${distro}" in
-        Fedora*)
-          url="https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64"
-          ext="rpm"
-          sudo dnf -y install exa bat the_silver_searcher tmux vim zsh;;
-        *)  url="";;
-      esac
-    elif [ "$machine" == "Darwin" ]; then
-      brew install --cask visual-studio-code
-    fi
-
-    setupCode "${url}" "${ext}"
-    if [ ! -d ~/.asdf ]; then
-      git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
-    fi
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    export PATH="$(python3 -m site --user-base)/bin:$PATH"
+    pip3 install --user ansible
 }
 
-function setupCode {
-  local url=$1
-  local ext=$2
-  if [ "${url}" != "" ] && [ ! -f "code.${ext}" ]; then
-    wget -O "code.${ext}" "${url}"
-    if [ "${ext}" == "rpm" ]; then
-      sudo rpm -Uvh code.rpm
+function installXCodeTools {
+    echo "Checking Command Line Tools for Xcode"
+    # Only run if the tools are not installed yet
+    # To check that try to print the SDK path
+    xcode-select -p &> /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Command Line Tools for Xcode not found. Installing from softwareupdate…"
+
+      # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+      touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+
+      PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
+      softwareupdate -i "$PROD" --verbose;
+    else
+      echo "Command Line Tools for Xcode have been installed."
     fi
-  fi
 }
 
-# Get Platform
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine="Linux" ;;
-    Darwin*)    machine="Mac" ;;
-    CYGWIN*)    machine="Cygwin" ;;
-    MINGW*)     machine="MinGw" ;;
-    *)          machine="UNKNOWN:${unameOut}" ;;
-esac
 
-setupDeps
-setupZsh
-setupVim
+prepare
