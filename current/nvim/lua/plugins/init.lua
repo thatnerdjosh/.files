2e991e0de486073f9189359ef0b4cb88839a79f3
Josh Santos <josh@santos.cloud>
2024-08-20 17:50:42 +0700

    Add journal template

--- current/nvim/lua/plugins/init.lua
+++ current/nvim/lua/plugins/init.lua
@@ -9,8 +9,13 @@ return
 			require('orgmode').setup({
 				org_agenda_files = '~/orgfiles/**/*',
 				org_default_notes_file = '~/orgfiles/refile.org',
+				org_capture_templates = {
+					j = {
+						description = 'Journal',
+						template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
+					}
+				}
 			})
-
 			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
 			-- add ~org~ to ignore_install
 			-- require('nvim-treesitter.configs').setup({
